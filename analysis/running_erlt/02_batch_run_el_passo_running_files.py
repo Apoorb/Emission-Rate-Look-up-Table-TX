401d150666ae36cb222c805a7e8541efb09649b6
Apoorba Bibeka <apoorb2510@gmail.com>
2021-01-26 22:05:00 -0600

    Delete class object. Call destructor.

--- analysis/running_erlt/02_batch_run_el_passo_running_files.py
+++ analysis/running_erlt/02_batch_run_el_passo_running_files.py
@@ -8,37 +8,41 @@ import os
 import logging
 import time
 import datetime
-from ttierlt.utils import PATH_INTERIM, get_db_nm_list
-from ttierlt.utils import create_qaqc_output_conflicted_schema
+from ttierlt.utils import PATH_INTERIM, get_db_nm_list, create_qaqc_output_conflicted_schema
 from ttierlt.running.batch_sql import create_running_table_in_db
 from ttierlt.running.batch_sql import RunningSqlCmds as erlt_running
 
-DEBUG = True
+DEBUG = False
 
 if __name__ == "__main__":
+    # Set logging file details.
     path_to_log_dir = os.path.join(PATH_INTERIM, "Log Files")
     if not os.path.exists(path_to_log_dir):
         os.mkdir(path_to_log_dir)
     logfilenm = datetime.datetime.now().strftime('log_02_batch_run_el_passo_running_files_%H_%M_%d_%m_%Y.log')
     path_log_file = os.path.join(path_to_log_dir, logfilenm)
     logging.basicConfig(filename=path_log_file, filemode='w', level=logging.INFO)
+    # # Get list of processed databases.
+    # TODO: Inventory and skip processed files.
+    # Create output schemas and running output table.
+    create_qaqc_output_conflicted_schema()
+    # Delete the existing output table. It cannot have duplicated data; will raise error if you try to add
+    # duplicated data.
+    create_running_table_in_db(delete_if_exists=True)
+
     db_nms_list = get_db_nm_list(county_abb="elp")
-    list_erlt_dfs = []
-    STOP_ITER = 2
+    STOP_ITER = 10
     ITER_CNTER = 0
     for db_nm in db_nms_list:
         start_time = time.time()
-        if DEBUG & ITER_CNTER == STOP_ITER:
+        if DEBUG & (ITER_CNTER == STOP_ITER):
             break
         ITER_CNTER = ITER_CNTER + 1
-        create_qaqc_output_conflicted_schema()
-        # Delete the existing output table. It cannot have duplicated data; will raise error if you try to add
-        # duplicated data.
-        create_running_table_in_db(delete_if_exists=True)
+
         # Run the SQL Commands on the database.
         ########################################################
         logging.info(f"# Start processing {db_nm}")
-        print("# Start processing {db_nm}")
+        print(f"# Start processing {db_nm}")
         print("-------------------------------------------------------------------")
         erlt_running_obj = erlt_running(
             db_nm_=db_nm,
@@ -57,4 +61,5 @@ if __name__ == "__main__":
         logging.info(f"# End processing {db_nm}")
         print("---Query execution time:  %s seconds ---" % (time.time() - query_start_time))
         print(f"# End processing {db_nm}")
-        print("-------------------------------------------------------------------")
+        print("------------------------------------------------------------------------------------------")
+        del erlt_running_obj
