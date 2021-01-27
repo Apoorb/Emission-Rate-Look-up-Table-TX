c80e4dbae2e3e5b2c0352cb016ed8092f4e45a94
Apoorba Bibeka <apoorb2510@gmail.com>
2021-01-27 15:49:20 -0600

    Update schema creation and result table deletion.

--- analysis/running_erlt/00_setting_up_schemas.py
+++ analysis/running_erlt/00_setting_up_schemas.py
@@ -4,12 +4,21 @@ Created by: Apoorba Bibeka
 Date Created: 01/25/2021
 """
 from ttierlt.utils import connect_to_server_db, get_db_nm_list, create_qaqc_output_conflicted_schema
-
+from ttierlt.running.batch_sql import create_running_table_in_db
 
 conn = connect_to_server_db(database_nm=None)
 cur = conn.cursor()
 # Create Output database if not exist
 create_qaqc_output_conflicted_schema()
+
+# Delete the existing output table. It cannot have duplicated data; will raise error if you try to add
+# duplicated data.
+delete_running_table_if_exists = input("Do you want to loose the data from previous runs and create a fresh table?(y/n)")
+delete_if_exists_user_input = False
+if delete_running_table_if_exists.lower() == "y":
+    print("Change the delete_if_exists_user_input parameter to True. I am intentionally not automating it.")
+    delete_if_exists_user_input = False
+create_running_table_in_db(delete_if_exists=delete_if_exists_user_input)
 
 # Clean-up existing intermediate tables.
 for db_nm in get_db_nm_list("elp"):
