e20e7f380a5e7a2ba8ca2c49b267448647b25230
Apoorba Bibeka <apoorb2510@gmail.com>
2021-01-28 12:00:00 -0600

    Update test to explicitly list parameters instead of assuming and automating things.

--- tests/test_data_batch_run_el_passo_running.py
+++ tests/test_data_batch_run_el_passo_running.py
@@ -7,13 +7,35 @@ import pandas as pd
 import numpy as np
 from ttierlt.utils import connect_to_server_db
 
+@pytest.fixture(scope="session")
+def get_pollutant_cols():
+    return [
+                "CO",
+                "NOX",
+                "SO2",
+                "NO2",
+                "VOC",
+                "CO2EQ",
+                "PM10",
+                "PM25",
+                "BENZ",
+                "NAPTH",
+                "BUTA",
+                "FORM",
+                "ACTE",
+                "ACROL",
+                "ETYB",
+                "DPM",
+                "POM",
+            ]
 
 @pytest.fixture(scope="session")
 def get_erlt_running_2014b_data_py(request):
     conn = connect_to_server_db(database_nm="mvs2014b_erlt_out")
-    erlt_df_2014b_py = pd.read_sql(f"SELECT * FROM {request.param}", conn)
+    erlt_df_2014b_py = pd.read_sql(f"SELECT * FROM {request.param['data']}", conn)
+    erlt_df_2014b_py_fil = erlt_df_2014b_py.loc[lambda df: df.Area.isin(request.param["fil_county"])]
     conn.close()
-    return erlt_df_2014b_py
+    return erlt_df_2014b_py_fil
 
 
 @pytest.fixture(scope="session")
@@ -26,7 +48,6 @@ def get_qaqc_tables_created_from_sql():
     table_names = [
         table[0] for table in tables_in_qaqc if re.search(search_pat, table[0])
     ]
-    erlt_df_subsets_sql = pd.DataFrame()
     list_qaqc_dfs = []
     for table_nm in table_names:
         list_qaqc_dfs.append(pd.read_sql(f"SELECT * FROM {table_nm}", conn))
@@ -35,65 +56,34 @@ def get_qaqc_tables_created_from_sql():
     conn.close()
 
 
-def get_erlt_running_2014b_data_py_non_fixture():
-    conn = connect_to_server_db(database_nm="mvs2014b_erlt_out")
-    erlt_df_2014b_py = pd.read_sql("SELECT * FROM running_erlt_intermediate", conn)
-    conn.close()
-    return erlt_df_2014b_py
-
-
-def get_qaqc_tables_created_from_sql_non_fixture():
-    conn = connect_to_server_db(database_nm="mvs2014b_erlt_qaqc")
-    cur = conn.cursor()
-    cur.execute(" SHOW TABLES")
-    tables_in_qaqc = cur.fetchall()
-    search_pat = re.compile(".*qaqc_from_orignal$")
-    table_names = [
-        table[0] for table in tables_in_qaqc if re.search(search_pat, table[0])
-    ]
-    erlt_df_subsets_sql = pd.DataFrame()
-    list_qaqc_dfs = []
-    for table_nm in table_names:
-        list_qaqc_dfs.append(pd.read_sql(f"SELECT * FROM {table_nm}", conn))
-    erlt_df_subsets_sql = pd.concat(list_qaqc_dfs)
-    return erlt_df_subsets_sql
-    conn.close()
-
-
-def get_py_sql_df_list(df_py, df_sql):
-    group_area_yearId_monthid_py = df_py.groupby(["Area", "yearid", "monthid"])
-    group_area_yearId_monthid_sql = df_sql.groupby(["Area", "yearid", "monthid"])
-    sql_py_df_list = []
-    for sql_group_key, sql_erlt_df in group_area_yearId_monthid_sql:
-        sql_group_key_nm = (
-            str(sql_group_key[0])
-            + "_"
-            + str(sql_group_key[1])
-            + "_"
-            + str(sql_group_key[2])
-        )
-        py_erlt_df_v1 = group_area_yearId_monthid_py.get_group(
-            sql_group_key
-        ).reset_index(drop=True)
-        sql_erlt_df_v1 = sql_erlt_df.reset_index(drop=True)
-        sql_py_df_list.append((sql_erlt_df_v1, py_erlt_df_v1, sql_group_key_nm))
-    return sql_py_df_list
+@pytest.fixture(scope="session")
+def get_py_sql_df_list(get_erlt_running_2014b_data_py, get_qaqc_tables_created_from_sql, request):
+    group_area_yearId_monthid_py = get_erlt_running_2014b_data_py.groupby(["Area", "yearid", "monthid"])
+    group_area_yearId_monthid_sql = get_qaqc_tables_created_from_sql.groupby(["Area", "yearid", "monthid"])
+    py_erlt_df_fil = group_area_yearId_monthid_py.get_group(request.param["grp_key"]).reset_index(drop=True)
+    sql_erlt_df_fil = group_area_yearId_monthid_sql.get_group(request.param["grp_key"]).reset_index(drop=True)
+    return {"py_erlt_df_fil": py_erlt_df_fil, "sql_erlt_df_fil": sql_erlt_df_fil, "grp_key": request.param["grp_key"]}
 
 
 @pytest.mark.parametrize(
-    "sql_df, py_df, sql_group_key_nm",
-    get_py_sql_df_list(
-        get_erlt_running_2014b_data_py_non_fixture(),
-        get_qaqc_tables_created_from_sql_non_fixture(),
-    ),
+    "get_erlt_running_2014b_data_py, get_py_sql_df_list",
+    [
+        ({"data" : "running_erlt_intermediate", "fil_county": ["El Paso"]}, {"grp_key": ("El Paso", 2020, 1)}),
+        ({"data": "running_erlt_intermediate", "fil_county": ["El Paso"]}, {"grp_key": ("El Paso", 2022, 7)}),
+        ({"data": "running_erlt_intermediate", "fil_county": ["El Paso"]}, {"grp_key": ("El Paso", 2024, 10)}),
+        ({"data": "running_erlt_intermediate", "fil_county": ["El Paso"]}, {"grp_key": ("El Paso", 2044, 4)}),
+    ],
+    ids=["_".join(map(str, ("El Paso", 2020, 1))), "_".join(map(str, ("El Paso", 2022, 7))),
+         "_".join(map(str, ("El Paso", 2024, 10))), "_".join(map(str, ("El Paso", 2044, 4)))],
+    indirect=True
 )
-def test_final_running_erlt_matches_between_py_sql_v1(sql_df, py_df, sql_group_key_nm):
-    print(sql_group_key_nm)
-    pd.testing.assert_frame_equal(sql_df, py_df)
+def test_final_running_erlt_matches_between_py_sql_v1(get_erlt_running_2014b_data_py, get_py_sql_df_list):
+    print(get_py_sql_df_list["grp_key"])
+    pd.testing.assert_frame_equal(get_py_sql_df_list["py_erlt_df_fil"], get_py_sql_df_list["sql_erlt_df_fil"])
 
 
 @pytest.mark.parametrize(
-    "get_erlt_running_2014b_data_py", ["running_erlt_intermediate"], indirect=True
+    "get_erlt_running_2014b_data_py", [{"data" : "running_erlt_intermediate", "fil_county": ["El Paso"]}], indirect=True
 )
 def test_unique_groups_by_area_year_rdtype_in_erlt_2014b_data(
     get_erlt_running_2014b_data_py,
@@ -105,7 +95,7 @@ def test_unique_groups_by_area_year_rdtype_in_erlt_2014b_data(
 
 
 @pytest.mark.parametrize(
-    "get_erlt_running_2014b_data_py", ["running_erlt_intermediate"], indirect=True
+    "get_erlt_running_2014b_data_py", [{"data" : "running_erlt_intermediate", "fil_county": ["El Paso"]}], indirect=True
 )
 def test_unique_avg_speed_2_5_to_75(get_erlt_running_2014b_data_py):
     assert all(
@@ -126,7 +116,7 @@ def test_unique_avg_speed_2_5_to_75(get_erlt_running_2014b_data_py):
 
 
 @pytest.mark.parametrize(
-    "get_erlt_running_2014b_data_py", ["running_erlt_intermediate"], indirect=True
+    "get_erlt_running_2014b_data_py", [{"data" : "running_erlt_intermediate", "fil_county": ["El Paso"]}], indirect=True
 )
 def test_unique_funclass(get_erlt_running_2014b_data_py):
     assert all(
@@ -150,7 +140,7 @@ def test_unique_funclass(get_erlt_running_2014b_data_py):
 
 
 @pytest.mark.parametrize(
-    "get_erlt_running_2014b_data_py", ["running_erlt_intermediate"], indirect=True
+    "get_erlt_running_2014b_data_py", [{"data" : "running_erlt_intermediate", "fil_county": ["El Paso"]}], indirect=True
 )
 def test_unique_yearid(get_erlt_running_2014b_data_py):
     assert all(
@@ -169,7 +159,7 @@ def test_unique_yearid(get_erlt_running_2014b_data_py):
 
 
 @pytest.mark.parametrize(
-    "get_erlt_running_2014b_data_py", ["running_erlt_intermediate"], indirect=True
+    "get_erlt_running_2014b_data_py", [{"data" : "running_erlt_intermediate", "fil_county": ["El Paso"]}], indirect=True
 )
 def test_unique_monthid(get_erlt_running_2014b_data_py):
     assert all(
@@ -190,36 +180,19 @@ def test_unique_monthid(get_erlt_running_2014b_data_py):
 @pytest.mark.parametrize(
     "get_erlt_running_2014b_data_py, quantile_unique",
     [
-        ("running_erlt_intermediate", 1),
-        ("running_erlt_intermediate_yr_interpolated", 1),
-        ("running_erlt_intermediate_yr_interpolated_spd_interpolated", 1),
+        ({"data" : "running_erlt_intermediate", "fil_county": ["El Paso"]}, 1),
+        ({"data" : "running_erlt_intermediate_yr_interpolated", "fil_county": ["El Paso"]}, 1),
+        ({"data" : "running_erlt_intermediate_yr_spd_interpolated_no_monthid", "fil_county": ["El Paso"]}, 1),
     ],
+    ids = ["running_erlt_intermediate", "running_erlt_intermediate_yr_interpolated", "running_erlt_intermediate_yr_spd_interpolated_no_monthid"],
     indirect=["get_erlt_running_2014b_data_py"],
 )
 def test_unique_values_percent_unique_pollutants(
-    get_erlt_running_2014b_data_py, quantile_unique
+    get_erlt_running_2014b_data_py, quantile_unique, get_pollutant_cols
 ):
     num_unique_emmision_rates_pollutants = (
         get_erlt_running_2014b_data_py[
-            [
-                "CO",
-                "NOX",
-                "SO2",
-                "NO2",
-                "VOC",
-                "CO2EQ",
-                "PM10",
-                "PM25",
-                "BENZ",
-                "NAPTH",
-                "BUTA",
-                "FORM",
-                "ACTE",
-                "ACROL",
-                "ETYB",
-                "DPM",
-                "POM",
-            ]
+            get_pollutant_cols
         ]
         .nunique()
         .values
@@ -227,3 +200,20 @@ def test_unique_values_percent_unique_pollutants(
     assert np.quantile(num_unique_emmision_rates_pollutants, quantile_unique) == len(
         get_erlt_running_2014b_data_py
     )
+
+
+@pytest.mark.parametrize(
+    "get_erlt_running_2014b_data_py, min_val",
+    [
+        ({"data" : "running_erlt_intermediate", "fil_county": ["El Paso"]}, 0),
+        ({"data" : "running_erlt_intermediate_yr_interpolated", "fil_county": ["El Paso"]}, 0),
+        ({"data" : "running_erlt_intermediate_yr_spd_interpolated_no_monthid", "fil_county": ["El Paso"]}, 0),
+    ],
+    ids=["running_erlt_intermediate", "running_erlt_intermediate_yr_interpolated",
+         "running_erlt_intermediate_yr_spd_interpolated_no_monthid"],
+    indirect=["get_erlt_running_2014b_data_py"],
+)
+def test_min_values_over_zero_pollutants(
+    get_erlt_running_2014b_data_py, min_val, get_pollutant_cols
+):
+    get_erlt_running_2014b_data_py[get_pollutant_cols].min() > 0
